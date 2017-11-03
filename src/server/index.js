fdacdbe73d47dd17760d02995203d118027ad2a7
merlox <merloxdixcontrol@gmail.com>
2017-11-03 21:50:56 +0100

    Made some small changes to the server code

--- src/server/index.js
+++ src/server/index.js
@@ -1,45 +1,45 @@
-const express = require('express');
-const favicon = require('serve-favicon');
+const express = require('express')
+const favicon = require('serve-favicon')
 
 module.exports = (async function () {
-  let leverj = {};
-  let app = express();
-  let compress = require('compression');
-  let server = getServer();
-  let helmet = require("helmet");
+  let leverj = {}
+  let app = express()
+  let compress = require('compression')
+  let server = getServer()
+  let helmet = require('helmet')
 
   function getServer() {
     return require('http').Server(app)
   }
 
-  app.use(helmet({frameguard: {action: 'deny'}}));
-  app.use(helmet.noCache());
-  app.use(helmet.xssFilter());
+  app.use(helmet({frameguard: {action: 'deny'}}))
+  app.use(helmet.noCache())
+  app.use(helmet.xssFilter())
 
-  app.use(compress());
+  app.use(compress())
 
-  let indexhtml = "index.html";
-  app.use(express.static('./dist/src/client', {maxAge: 31536000000}));
+  let indexhtml = 'index.html'
+  app.use(express.static('./dist/src/client', {maxAge: 31536000000}))
 
   app.get(['/'], function (req, res) {
     return res.sendFile(indexhtml, {root: './dist/src/client'})
-  });
+  })
 
   app.use(function (err, req, res, next) {
-    util.log('FAIL', err);
-    util.log('FAIL, stack:', err.stack);
+    util.log('FAIL', err)
+    util.log('FAIL, stack:', err.stack)
     res.status(err.statusCode || 500).send({error: err.message})
-  });
+  })
 
   function init() {
-    server.listen(8888, '0.0.0.0');
+    server.listen(8888, '0.0.0.0')
     process.on('uncaughtException', (err) => {
-      console.log("################################## uncaught exception ######################################");
-      util.log(err.stack);
-      console.log("################################## uncaught exception ######################################")
-    });
+      console.log('################################## uncaught exception ######################################')
+      util.log(err.stack)
+      console.log('################################## uncaught exception ######################################')
+    })
   }
 
-  init();
-  return leverj;
-})();
+  init()
+  return leverj
+})()
