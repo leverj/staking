37a76d28a09f955ae72330f37a202be76c232ace
merlox <merloxdixcontrol@gmail.com>
2017-11-17 04:40:07 +0830

    Removed buyTokens

--- scripts/testStakeAutomation.js
+++ scripts/testStakeAutomation.js
@@ -5,11 +5,12 @@ const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
 const stakeABI = require('./../build/contracts/Stake.json');
 const testrpc = require('./../testrpc.js')
 const App = require('./stakeAutomation.js')
+let app
 
 describe('App', () => {
 	before(async () => {
 		testrpc.start()
-		
+		app = new App()
 	})
 
 	it('should create the operator from the private key', async () => {
