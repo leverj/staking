17eec8624dcabe2b69f7710d26edc675d88908d8
Nirmal Gupta <nirmalk.gupta@gmail.com>
2017-11-23 00:28:26 +0530

    automation.

---
+++ scripts/stake.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+export ADDRESS=0xDaa1A6c972d4b8d57dce119E48C6ABB19BaF8402
+export PRIVATEKEY=0x1485e9840653a6230b94c8a83310c5001a7c09405aca874ee76a6633879fae4c
+export STAKE=0x24ff28af80967625241848550711f70360b98689
+export PROVIDER=https://ropsten.infura.io
+export SOCKETPROVIDER=ws://51.15.134.155:8546
+export BLOCKINTERVAL=10
+export GAS=3e5
+
+node automation.js
