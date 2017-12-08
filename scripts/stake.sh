56c07d1999393d92c41fb07f71118c88c494c9a8
Nirmal Gupta <nirmalk.gupta@gmail.com>
2017-12-08 23:39:19 +0530

    Delete stake.sh

--- scripts/stake.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-export STAKE=0x21ec0699AE84e342D34cABA2628880901e1A2bF1
-export PROVIDER=https://ropsten.infura.io
-export SOCKETPROVIDER=ws://51.15.134.155:8546
-export BLOCKINTERVAL=10
-export GAS=3e5
-
-node automation.js ../../../coinpit.io/ignore/privateKeys/myEtherWallet/0xDaa1A6c972d4b8d57dce119E48C6ABB19BaF8402.json
