2fed3cefb24d61160d62aa32405ba41c012709cf
Nirmal Gupta <nirmalk.gupta@gmail.com>
2018-03-26 15:53:23 -0700

    added migration.

--- deployment/migrateFeeTokens.js
+++ deployment/migrateFeeTokens.js
@@ -10,35 +10,36 @@ module.exports = function () {
     affirm(web3.utils.isAddress(_previousFeeAddress), 'Invalid previous address')
     affirm(web3.utils.isAddress(_newFeeAddress), 'Invalid new address')
     affirm(_abi, 'Invalid _abi')
-    previousFeeAddress      = _previousFeeAddress
-    newFeeAddress           = _newFeeAddress
-    abi                     = _abi
-    deploymentUtil          = _deploymentUtil
-    previousFee             = new web3.eth.Contract(_abi, _previousFeeAddress)
-    newFee                  = new web3.eth.Contract(_abi, _newFeeAddress)
-    let recipientAddressMap = await getTokenRecipient()
-    console.log('setting deployer as minter')
-    await deploymentUtil.sendTx(newFee, newFee.methods.setMinter(deploymentUtil.getDeployer().address))
-
-    let recipientAddresses = Object.keys(recipientAddressMap);
+    previousFeeAddress = _previousFeeAddress
+    newFeeAddress      = _newFeeAddress
+    abi                = _abi
+    deploymentUtil     = _deploymentUtil
+    previousFee        = new web3.eth.Contract(_abi, _previousFeeAddress)
+    newFee             = new web3.eth.Contract(_abi, _newFeeAddress)
+    if (await newFee.methods.minter().call() !== deploymentUtil.getDeployer().address) {
+      console.log('setting deployer as minter', deploymentUtil.getDeployer().address)
+      await deploymentUtil.sendTx(newFee, newFee.methods.setMinter(deploymentUtil.getDeployer().address))
+    }
+    let recipientAddressToBalance = await getTokenRecipient()
+    let recipientAddresses        = Object.keys(recipientAddressToBalance);
     for (let i = 0; i < recipientAddresses.length; i++) {
       let recipientAddress = recipientAddresses[i];
-      let tokenBalance = await newFee.methods.balanceOf(recipientAddress).call()
-      if(tokenBalance === recipientAddressMap[recipientAddress]) continue
-      console.log(`sending ${recipientAddressMap[recipientAddress]} FEE to ${recipientAddress}`)
-      await _deploymentUtil.sendTx(newFee, newFee.methods.sendTokens(recipientAddress, recipientAddressMap[recipientAddress]))
+      let tokenBalance     = await newFee.methods.balanceOf(recipientAddress).call()
+      if (tokenBalance === recipientAddressToBalance[recipientAddress]) continue
+      console.log(`sending ${recipientAddressToBalance[recipientAddress]} FEE to ${recipientAddress}`)
+      await _deploymentUtil.sendTx(newFee, newFee.methods.sendTokens(recipientAddress, recipientAddressToBalance[recipientAddress]))
     }
   }
 
   async function getTokenRecipient() {
-    let events    = await previousFee.getPastEvents('Transfer', {fromBlock: 0, toBlock: 'latest'})
-    let addresses = {}
+    let events             = await previousFee.getPastEvents('Transfer', {fromBlock: 0, toBlock: 'latest'})
+    let addressesToBalance = {}
     for (let i = 0; i < events.length; i++) {
       let to = events[i].returnValues._to;
-      if (addresses[to]) return
-      addresses[to] = await previousFee.methods.balanceOf(to).call()
+      if (addressesToBalance[to]) continue
+      addressesToBalance[to] = await previousFee.methods.balanceOf(to).call()
     }
-    return addresses
+    return addressesToBalance
   }
 
   return migration
