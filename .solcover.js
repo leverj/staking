113b2360195f7ed8eef74d85bbb199133192dc67
Nirmal Gupta <nirmalk.gupta@gmail.com>
2018-10-05 18:13:01 -0700

    added coverage to solidity code.

--- .solcover.js
+++ .solcover.js
@@ -1,6 +1,3 @@
 module.exports = {
   skipFiles     : ["*Token.sol"],
-  compileCommand: '../node_modules/.bin/truffle compile -all',
-  testCommand   : 'truffle test --network coverage',
-  testrpcOptions: `--port 8555 -i coverage --noVMErrorsOnRPCResponse`
 }
